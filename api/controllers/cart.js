d11289eceb8c86d6ab0b23e221b9024dd2a4bc8a
shaanxd <shaahid.xd@gmail.com>
2019-08-26 22:36:02 +0530

    Add get and delete routes for cart items

--- api/controllers/cart.js
+++ api/controllers/cart.js
@@ -4,22 +4,15 @@ const Cart = require('../models/cart');
 
 exports.add_to_cart = async(req, res, next) => {
     try {
-        const {body: {
-            userId, productId, quantity
-        }} = req;
-        if(userId && productId && quantity) {
-            const user = await User.findOne({
-                where: {
-                    id: userId,
-                }
-            });
+        const {body: {productId, quantity}, userData: {id: userId}} = req;
+        if(productId && quantity) {
             const product = await Product.findOne({
                 where: {
                     id: productId,
                 }
             });
-            if (user && product) {
-                const createdCart = await Cart.create({
+            if (product) {
+                await Cart.create({
                     userId,
                     productId,
                     itemQuantity: quantity,
@@ -29,15 +22,17 @@ exports.add_to_cart = async(req, res, next) => {
                     where: {
                         userId,
                     },
-                    attributes: {exclude: ['createdAt', 'updatedAt', 'userId']},
-                    include: [{model: Product, as: 'product'}],
+                    attributes: {exclude: ['createdAt', 'updatedAt', 'userId', 'productId']},
+                    include: [{
+                        model: Product,
+                        as: 'product',
+                        attributes: {exclude: ['createdAt', 'updatedAt']},
+                    }],
                 });
-                res.status(200).json({
-                    cart: userCart,
-                })
+                res.status(200).json(userCart)
             } else {
                 res.status(500).json({
-                    message: 'Invalid details.',
+                    message: 'Invalid product.',
                 });
             }
         } else {
@@ -49,5 +44,68 @@ exports.add_to_cart = async(req, res, next) => {
         res.status(500).json({
             message: err.message,
         })
+    }
+}
+
+exports.get_cart = async(req, res, next) => {
+    try {
+        const {userData: {id}} = req;
+        const userCart = await Cart.findAll({
+            where: {
+                userId: id,
+            },
+            attributes: {exclude: ['createdAt', 'updatedAt', 'userId', 'productId']},
+            include: [{
+                model: Product,
+                as: 'product',
+                attributes: {exclude: ['createdAt', 'updatedAt']},
+            }],
+        });
+        res.status(200).json(userCart);
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+}
+
+exports.delete_cart = async(req, res, next) => {
+    try {
+        const {params: {id}, userData: {id: userId}} = req;
+
+        const cartItem = await Cart.findOne({
+            where: {
+                id,
+            }
+        });
+        if (cartItem) {
+            if(cartItem.userId === userId) {
+                await cartItem.destroy();
+                const userCart = await Cart.findAll({
+                    where: {
+                        userId,
+                    },
+                    attributes: {exclude: ['createdAt', 'updatedAt', 'userId', 'productId']},
+                    include: [{
+                        model: Product,
+                        as: 'product',
+                        attributes: {exclude: ['createdAt', 'updatedAt']},
+                    }],
+                });
+                res.status(200).json(userCart);
+            } else {
+                res.status(401).json({
+                    message: 'Not authorized to remove cart item',
+                });
+            }
+        } else {
+            res.status(404).json({
+                message: 'Could not find cart item.',
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
     }
 }
--- api/routes/cart.js
+++ api/routes/cart.js
@@ -7,4 +7,8 @@ const router = express.Router();
 
 router.post('/addtocart', checkAuth, cartController.add_to_cart);
 
+router.get('/', checkAuth, cartController.get_cart);
+
+router.delete('/:id', checkAuth, cartController.delete_cart);
+
 module.exports = router;
